105de3bc8b611760fa4b77848f2f230af5c199f9
Kerri Morris <kkmorrisfam@hotmail.com>
2025-09-01 10:12:22 -0700

    Initialize, beginning code with some copies to be edited

---
+++ src/render.cpp
@@ -0,0 +1,148 @@
+#include <iostream>
+
+#include <fmt/format.h>
+#include <imgui.h>
+#include <implot.h>
+
+#include "render.hpp"
+
+void WindowClass::Draw(std::string_view label)
+{
+    constexpr static auto window_flags =
+        ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove |
+        ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoScrollbar;
+    constexpr static auto window_size = ImVec2(1280.0F, 720.0F);
+    constexpr static auto window_pos = ImVec2(0.0F, 0.0F);
+
+    ImGui::SetNextWindowSize(window_size);
+    ImGui::SetNextWindowPos(window_pos);
+
+    ImGui::Begin(label.data(), nullptr, window_flags);
+
+    ImGui::End();
+}
+
+void WindowClass::DrawSizeButtons()
+{
+
+}
+
+void WindowClass::DrawIoButtons()
+{
+
+}
+
+void WindowClass::DrawTable()
+{
+
+}
+
+void WindowClass::DrawSavePopup()
+{
+    const auto esc_pressed = ImGui::IsKeyPressed(ImGuiKey_Escape);
+
+    SetPopupLayout();
+
+    if (ImGui::BeginPopupModal("Save File", nullptr, popUpFlags))
+    {
+        ImGui::InputText("Filename", filenameBuffer, sizeof(filenameBuffer));
+
+        if (ImGui::Button("Save", popUpButtonSize))
+        {
+            SaveToCsvFile(filenameBuffer);
+            ImGui::CloseCurrentPopup();
+        }
+
+        ImGui::SameLine();
+
+        if (ImGui::Button("Cancel", popUpButtonSize) || esc_pressed)
+        {
+            ImGui::CloseCurrentPopup();
+        }
+
+        ImGui::EndPopup();
+    }
+}
+
+void WindowClass::DrawLoadPopup()
+{
+    const auto esc_pressed = ImGui::IsKeyPressed(ImGuiKey_Escape);
+
+    SetPopupLayout();
+
+    if (ImGui::BeginPopupModal("Load File", nullptr, popUpFlags))
+    {
+        ImGui::InputText("Filename", filenameBuffer, sizeof(filenameBuffer));
+
+        if (ImGui::Button("Load", popUpButtonSize))
+        {
+            LoadFromCsvFile(filenameBuffer);
+            ImGui::CloseCurrentPopup();
+        }
+
+        ImGui::SameLine();
+
+        if (ImGui::Button("Cancel", popUpButtonSize) || esc_pressed)
+        {
+            ImGui::CloseCurrentPopup();
+        }
+
+        ImGui::EndPopup();
+    }
+}
+
+void WindowClass::DrawValuePopup(const int row, const int col)
+{
+    const auto esc_pressed =
+        ImGui::IsKeyPressed(ImGui::GetKeyIndex(ImGuiKey_Escape));
+
+    SetPopupLayout();
+    if (ImGui::BeginPopupModal("Save File", nullptr, popUpFlags))
+    {
+        ImGui::InputText("Filename", filenameBuffer, sizeof(filenameBuffer));
+
+        if (ImGui::Button("Save", popUpButtonSize))
+        {
+            SaveToCsvFile(filenameBuffer);
+            ImGui::CloseCurrentPopup();
+        }
+
+        ImGui::SameLine();
+
+        if (ImGui::Button("Cancel", popUpButtonSize) || esc_pressed)
+        {
+            ImGui::CloseCurrentPopup();
+        }
+
+        ImGui::EndPopup();
+    }
+}
+
+void WindowClass::SaveToCsvFile(std::string_view filename)
+{
+
+}
+
+void WindowClass::LoadFromCsvFile(std::string_view filename)
+{
+
+}
+
+template <typename T>
+void WindowClass::PlotCellValue(std::string_view formatter, const T value)
+{
+
+}
+
+void WindowClass::SetPopupLayout()
+{
+    ImGui::SetNextWindowSize(popUpSize);
+    ImGui::SetNextWindowPos(popUpPos);
+
+}
+
+void render(WindowClass &window_obj)
+
+{
+    window_obj.Draw("Label");
+}
